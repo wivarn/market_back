3629936ecde084760d8147a3a0923eb9c447ff23
wivarn <wivarn@gmail.com>
2021-05-01 21:46:31 -0700

    add missing file

---
+++ app/racks/auth.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'sequel'
+require 'bcrypt'
+
+class Auth < Roda
+  DB = Sequel.connect('postgresql://', extensions: :activerecord_connection)
+  # plugin :middleware
+
+  plugin :rodauth, json: :only do
+    enable :login, :logout, :jwt
+    # enable :create_account, :verify_account, :verify_account_grace_period,
+    #        :login, :logout, :jwt,
+    #        :reset_password, :change_password, :change_password_notify,
+    #        :change_login, :verify_login_change,
+    #        :close_account
+
+    # account_password_hash_column :password_hash
+
+    # jwt_secret '657e57e5784301eeab3dcbfef181d6b86d5c97eb3dd2770ee89f1b656248311c068e45a46e796d254be3cbacfaa96da60426696c99a68d4d5a3978a2b6d4b2d3'
+
+    # account_status_column :status
+    # account_unverified_status_value 'unverified'
+    # account_open_status_value 'verified'
+    # account_closed_status_value 'closed'
+
+    # verify_account_set_password? false
+  end
+
+  route do |r|
+    env['rodauth'] = rodauth
+    r.rodauth
+  end
+end
+
+# run Auth.app if __FILE__ == $0

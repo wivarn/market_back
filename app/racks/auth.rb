8e962219ad39e6440a416425c384ea3304bd21f3
wivarn <wivarn@gmail.com>
2021-05-02 01:47:51 -0700

    update configs

--- app/racks/auth.rb
+++ app/racks/auth.rb
@@ -9,23 +9,22 @@ class Auth < Roda
   # plugin :middleware
 
   plugin :rodauth, json: :only do
-    enable :login, :logout, :jwt
-    # enable :create_account, :verify_account, :verify_account_grace_period,
-    #        :login, :logout, :jwt,
-    #        :reset_password, :change_password, :change_password_notify,
-    #        :change_login, :verify_login_change,
-    #        :close_account
+    enable :create_account, :verify_account, :verify_account_grace_period,
+           :login, :logout, :jwt,
+           :reset_password, :change_password, :change_password_notify,
+           :change_login, :verify_login_change
 
     # account_password_hash_column :password_hash
+    use_database_authentication_functions? false
 
-    # jwt_secret '657e57e5784301eeab3dcbfef181d6b86d5c97eb3dd2770ee89f1b656248311c068e45a46e796d254be3cbacfaa96da60426696c99a68d4d5a3978a2b6d4b2d3'
+    jwt_secret '657e57e5784301eeab3dcbfef181d6b86d5c97eb3dd2770ee89f1b656248311c068e45a46e796d254be3cbacfaa96da60426696c99a68d4d5a3978a2b6d4b2d3'
 
-    # account_status_column :status
-    # account_unverified_status_value 'unverified'
-    # account_open_status_value 'verified'
+    account_status_column :status
+    account_unverified_status_value 'unverified'
+    account_open_status_value 'verified'
     # account_closed_status_value 'closed'
 
-    # verify_account_set_password? false
+    verify_account_set_password? false
   end
 
   route do |r|

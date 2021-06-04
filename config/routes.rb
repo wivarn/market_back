60f93a8e527fcef8ddbb77f9d00540c8bd444a3c
wivarn <wivarn@gmail.com>
2021-06-04 16:43:11 -0700

    prefix everything with v0

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 Jets.application.routes.draw do
-  resources :listings, param: :id do
-    get :search, on: :collection
-  end
-  resource :profile, only: %i[show update]
-  root 'jets/public#show'
-  mount Auth, at: 'auth'
+  prefix :v0 do
+    resources :listings, param: :id do
+      get :search, on: :collection
+    end
+    resource :profile, only: %i[show update]
 
-  # The jets/public#show controller can serve static utf8 content out of the public folder.
-  # Note, as part of the deploy process Jets uploads files in the public folder to s3
-  # and serves them out of s3 directly. S3 is well suited to serve static assets.
-  # More info here: https://rubyonjets.com/docs/extras/assets-serving/
-  any '*catchall', to: 'jets/public#show'
+    mount Auth, at: 'auth'
+  end
 end

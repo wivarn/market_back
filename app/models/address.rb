7fb7f73d56d59b5c8f3e882cec6c66a8a8bc052f
wivarn <wivarn@gmail.com>
2021-06-07 19:52:32 -0700

    add validations to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,20 @@
 class Address < ApplicationRecord
+  STATE_CODES = %w[
+    AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO
+    MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY
+  ].freeze
+
+  PROVINCE_CODES = %w[AB BC MB NB NL NT NS NU ON PE QC SK YT].freeze
+
   belongs_to :account
+
+  validates :street1, :city, :state, :zip, :country, presence: true
+  validates :street1, :city, length: { in: 1..100 }
+  validates :street2, length: { maximum: 100 }
+  validates :country, inclusion: { in: %w[USA CAN] }
+  validates :state, inclusion: { in: STATE_CODES, message: 'invalid state' }, if: -> { country == 'USA' }
+  validates :state, inclusion: { in: PROVINCE_CODES, message: 'invalid province' }, if: -> { country == 'CAN' }
+  validates :zip, format: { with: /\A\d{5}\z/, message: 'invalid zip code' }, if: -> { country == 'USA' }
+  validates :zip, format: { with: /\A[A-Z]\d[A-Z]\d[A-Z]\d\z/,
+                            message: 'invalid postal code' }, if: -> { country == 'CAN' }
 end

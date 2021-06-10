2799ba7eb3cb68cfa4e1e16d11ba5f16ec866ada
wivarn <wivarn@gmail.com>
2021-06-10 01:34:13 -0700

    update listing validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,12 +3,20 @@
 class Listing < ApplicationRecord
   validates :account, :photos, :title, :condition, :currency, :price, :domestic_shipping, :status, presence: true
   validates :title, length: { in: 2..100 }
-  validates :price, :domestic_shipping, :international_shipping,
-            format: { with: /\A\d{1,8}(\.\d{0,2})?\z/ },
+  validates :price, :domestic_shipping, format: { with: /\A\d{1,8}(\.\d{0,2})?\z/ }
+  validates :price, numericality: {
+    greater_than_or_equal_to: 0.25,
+    less_than: 100_000_000
+  }
+  validates :domestic_shipping, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than: 100_000_000
+  }
+  validates :international_shipping,
             numericality: {
               greater_than_or_equal_to: 0,
               less_than: 100_000_000
-            },
+            }, format: { with: /\A\d{1,8}(\.\d{0,2})?\z/ },
             allow_nil: true
 
   belongs_to :account

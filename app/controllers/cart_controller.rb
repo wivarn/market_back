00cabf4d4ee5c5e5febd4d61e56cc6e6631b8381
wivarn <wivarn@gmail.com>
2021-06-29 05:13:42 -0700

    checkout

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -19,7 +19,39 @@ class CartController < ApplicationController
     end
   end
 
-  def checkout; end
+  def checkout
+    listings = @cart.listings
+    total_price = 0
+    line_items = []
+    listings.each do |listing|
+      amount = (listing.price + listing.domestic_shipping) * 100
+      total_price += amount
+      line_items << {
+        name: listing.title,
+        amount: amount.to_i,
+        currency: listing.currency.downcase,
+        quantity: 1
+      }
+    end
+
+    application_fee_amount = total_price * 0.05
+
+    stripe_account = listings.first.account.stripe_connection.stripe_account
+
+    session = Stripe::Checkout::Session
+              .create({
+                        payment_method_types: ['card'],
+                        line_items: line_items,
+                        payment_intent_data: {
+                          application_fee_amount: application_fee_amount.to_i
+                        },
+                        mode: 'payment',
+                        success_url: "#{ENV['FRONT_END_BASE_URL']}/account/purchaseHistory",
+                        cancel_url: "#{ENV['FRONT_END_BASE_URL']}/cart"
+                      }, { stripe_account: stripe_account })
+
+    render json: session
+  end
 
   private
 

fe49815a091f6e7e6ade58f1e073fac6de2f7539
wivarn <wivarn@gmail.com>
2021-07-02 23:53:19 -0700

    fix connect with stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,22 +4,41 @@ class PaymentsController < ApplicationController
   before_action :authenticate!
 
   def show
-    render json: current_account.stripe_connection || {}
+    stripe_connection = current_account.stripe_connection
+    if stripe_connection
+      render json: Stripe::Account.retrieve(stripe_connection.stripe_account)
+    else
+      render json: {}, status: :no_content
+    end
   end
 
   def link_account
-    stripe_account = Stripe::Account.create(type: 'standard')
-    current_account.create_stripe_connection(stripe_account: stripe_account.id)
+    stripe_account = create_load_stripe_account
 
-    account_link = Stripe::AccountLink.create(
-      type: 'account_onboarding',
-      account: stripe_account.id,
-      refresh_url: "#{ENV['FRONT_END_BASE_URL']}/account/payments",
-      return_url: "#{ENV['FRONT_END_BASE_URL']}/account/payments"
-    )
+    if stripe_account.charges_enabled
+      render json: { error: 'You already have a linked Stripe Account' }, status: :conflict
+    else
+      account_link = Stripe::AccountLink.create(
+        type: 'account_onboarding',
+        account: stripe_account.id,
+        refresh_url: "#{ENV['FRONT_END_BASE_URL']}/account/payments",
+        return_url: "#{ENV['FRONT_END_BASE_URL']}/account/payments"
+      )
 
-    render json: { url: account_link.url }
+      render json: { url: account_link.url }
+    end
   end
 
-  # def link_account_refresh; end
+  private
+
+  def create_load_stripe_account
+    stripe_connection = StripeConnection.where(account: current_account).first_or_initialize
+    if stripe_connection.stripe_account
+      Stripe::Account.retrieve(stripe_connection.stripe_account)
+    else
+      stripe_account = Stripe::Account.create(type: 'standard', email: current_account.email)
+      stripe_connection.update(stripe_account: stripe_account.id)
+      stripe_account
+    end
+  end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+Stripe.log_level = Stripe::LEVEL_INFO

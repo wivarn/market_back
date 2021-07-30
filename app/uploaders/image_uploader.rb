0ba85a1a1df2fb018cfaa3e84212a2c5578ec294
wivarn <wivarn@gmail.com>
2021-07-29 21:57:12 -0700

    add locol url

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,6 +21,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  if Jets.env.development?
+    def url
+      super.gsub(ENV['FRONT_END_PUBLIC_PATH'], '')
+    end
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #

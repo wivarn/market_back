4ddfafb6b88e265ceb5859aa13e93051e3de84b8
wivarn <wivarn@gmail.com>
2021-07-31 14:24:30 -0700

    use_iam_profile

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
   else
     config.fog_credentials = {
       provider: 'AWS',
-      region: 'us-east-1'
+      region: 'us-east-1',
+      use_iam_profile: true
       # these values should only be set for local testing staging/prod will use IAM roles
       # aws_access_key_id: '',
       # aws_secret_access_key: '',

275e91a7a31350996e8f3b576334183ecd9d84e9
wivarn <wivarn@gmail.com>
2021-07-31 18:13:58 -0700

    add PUBLIC_ASSETS_URL

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Jets.application.configure do
-  config.gems.disable = false
+  config.gems.disable = true
   config.prewarm.enable = false
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :ses
 
-  # config.lambda.layers = [
-  #   'arn:aws:lambda:us-east-1:446093344105:layer:rubylayer:7'
-  # ]
+  config.lambda.layers = [
+    'arn:aws:lambda:us-east-1:446093344105:layer:rubylayer:8'
+  ]
 end

0f145c4337a89688b321ff914b5941b6e3d54766
wivarn <wivarn@gmail.com>
2021-08-11 11:41:06 -0700

    remove gems.disable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,13 +2,13 @@
 
 Jets.application.configure do
   # config.gems.disable = false
-  config.gems.disable = true
+  # config.gems.disable = true
   config.prewarm.enable = false
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :ses
 
-  config.lambda.layers = [
-    'arn:aws:lambda:us-east-1:446093344105:layer:rubylayer:10'
-  ]
+  # config.lambda.layers = [
+  #   'arn:aws:lambda:us-east-1:446093344105:layer:rubylayer:10'
+  # ]
 end

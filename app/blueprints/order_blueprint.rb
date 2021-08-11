4a9f6f529d8fd23619f8dc0e41b17aa596cc217b
wivarn <wivarn@gmail.com>
2021-08-11 02:03:59 -0700

    add missing file

---
+++ app/blueprints/order_blueprint.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class OrderBlueprint < ApplicationBlueprint
+  fields :aasm_state, :total, :tracking
+  association :buyer, blueprint: AccountBlueprint
+  association :seller, blueprint: AccountBlueprint
+  association :address, blueprint: AddressBlueprint
+end

89196b475a7a6035b33846106fb34d0f328b19fc
wivarn <wivarn@gmail.com>
2021-08-11 15:57:31 -0700

    guard against empty cart scenario

--- app/blueprints/cart_blueprint.rb
+++ app/blueprints/cart_blueprint.rb
@@ -5,23 +5,28 @@ class CartBlueprint < ApplicationBlueprint
   association :listings, blueprint: ListingBlueprint do |cart, options|
     destination = options[:destination_country]
     listings = cart.listings.to_a
+
     max_shipping_listing =
       listings.delete(listings.max_by { |listing| listing.shipping(destination_country: destination) })
     listings.each { |listing| listing.combined = true }
-    [max_shipping_listing] + listings
+    [max_shipping_listing, *listings].compact
   end
 
   field :total do |cart, options|
     destination = options[:destination_country]
     listings = cart.listings.to_a
-    max_shipping_listing =
-      listings.delete(listings.max_by { |listing| listing.shipping(destination_country: destination) })
 
-    total_shipping = listings.inject(max_shipping_listing.shipping(destination_country: destination)) do |sum, listing|
-      sum + listing.shipping(destination_country: destination, combined: true)
+    if listings.empty?
+      0
+    else
+      max_shipping_listing =
+        listings.delete(listings.max_by { |listing| listing.shipping(destination_country: destination) })
+      total_shipping = listings.inject(max_shipping_listing.shipping(destination_country: destination)) do |sum, listing|
+        sum + listing.shipping(destination_country: destination, combined: true)
+      end
+      total_listing_price = listings.inject(max_shipping_listing.price) { |sum, listing| sum + listing.price }
+
+      total_shipping + total_listing_price
     end
-    total_listing_price = listings.inject(max_shipping_listing.price) { |sum, listing| sum + listing.price }
-
-    total_shipping + total_listing_price
   end
 end

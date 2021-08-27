3162e449a5548363568fa410dd4aa96af4429a80
wivarn <wivarn@gmail.com>
2021-08-26 22:13:31 -0700

    use_iam_profile

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
   else
     config.fog_credentials = {
       provider: 'AWS',
-      region: 'us-east-1'
+      region: 'us-east-1',
+      use_iam_profile: true
       # need to replaces these
       # aws_access_key_id: ENV['AWS_ACCESS_ID'],
       # aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']

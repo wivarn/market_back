3c564f0db2e41a5af1b2d8f8f63b884ce3a2df87
wivarn <wivarn@gmail.com>
2021-09-06 00:14:20 -0700

    remove error reporting from dev and test env

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Rollbar.configure do |config|
-  config.access_token = 'b2e31c22149f4418a8a68db1f2d24a84'
+if defined? Rollbar
+  Rollbar.configure do |config|
+    config.access_token = 'b2e31c22149f4418a8a68db1f2d24a84'
+  end
 end

18f0b90582514fa75b4455507e99fc3f2f65d7fa
wivarn <wivarn@gmail.com>
2021-09-18 20:23:49 -0700

    move category to all ListingBlueprint

--- app/blueprints/listing_blueprint.rb
+++ app/blueprints/listing_blueprint.rb
@@ -2,7 +2,7 @@
 
 class ListingBlueprint < Blueprinter::Base
   identifier :id
-  fields :photos, :title, :grading_company, :condition, :currency, :price
+  fields :photos, :title, :grading_company, :condition, :currency, :price, :category
   field :aasm_state do |listing|
     listing.aasm_state == 'reserved' && listing.active? ? 'active' : listing.aasm_state
   end
@@ -11,12 +11,12 @@ class ListingBlueprint < Blueprinter::Base
   end
 
   view :seller do
-    fields :category, :subcategory, :tags, :description, :domestic_shipping, :international_shipping,
+    fields :description, :subcategory, :tags, :domestic_shipping, :international_shipping,
            :combined_shipping, :shipping_country, :reserved_at
   end
 
   view :buyer do
-    fields :description, :category, :subcategory, :combined_shipping, :reserved_at
+    fields :description, :subcategory, :combined_shipping, :reserved_at
     association :account, name: :seller, blueprint: AccountBlueprint, view: :with_location
   end
 end

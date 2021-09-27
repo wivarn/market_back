baecd89bdec60ac492a2f3eb7af65d7bbd5d1850
wivarn <wivarn@gmail.com>
2021-09-26 19:31:04 -0700

    typo

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -4,7 +4,7 @@ class Refund < ApplicationRecord
   STRIPE_REASONS = %w[duplicate fraudulent requested_by_customer].freeze
 
   validates :order, :refund_id, :amount, :status, presence: true
-  validate :reason, inclusion: { in: STRIPE_REASONS }, allow_nil: true
+  validates :reason, inclusion: { in: STRIPE_REASONS }, allow_nil: true
   validates :notes, presence: true, if: -> { !reason }
 
   belongs_to :order

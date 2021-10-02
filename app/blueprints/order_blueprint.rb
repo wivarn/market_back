2f3332d8df439d0dd2b8cb7b6c9884125c26b615
wivarn <wivarn@gmail.com>
2021-10-02 12:04:27 -0700

    update blueprint

--- app/blueprints/order_blueprint.rb
+++ app/blueprints/order_blueprint.rb
@@ -3,10 +3,14 @@
 class OrderBlueprint < Blueprinter::Base
   identifier :id
   fields :aasm_state, :total, :tracking, :created_at, :currency
+
+  field :refunded_total do |order|
+    order.refunds.inject(0) { |sum, refund| sum + refund.amount }
+  end
+
   association :buyer, blueprint: AccountBlueprint
   association :seller, blueprint: AccountBlueprint
   association :address, blueprint: AddressBlueprint
-  association :refunds, blueprint: RefundBlueprint
   association :listings, blueprint: ListingBlueprint do |order|
     listings = order.listings.to_a
     listings.each { |listing| listing.destination_country = order.address.country }
@@ -15,5 +19,6 @@ class OrderBlueprint < Blueprinter::Base
 
   view :with_history do
     fields :pending_shipment_at, :shipped_at, :received_at, :cancelled_at
+    association :refunds, blueprint: RefundBlueprint
   end
 end

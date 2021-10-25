161af68a3614d33d53b2dbcfad6694b82b4bf48b
wivarn <wivarn@gmail.com>
2021-10-25 08:39:17 -0700

    remove counter offer table

--- db/migrate/20211012073020_create_offers.rb
+++ db/migrate/20211012073020_create_offers.rb
@@ -22,27 +22,6 @@ class CreateOffers < ActiveRecord::Migration[6.1]
     add_index :offers, :buyer_id
     add_index :offers, :aasm_state
 
-    create_table :counter_offers do |t|
-      t.bigint :listing_id, null: false
-      t.bigint :buyer_id, null: false
-      t.string :aasm_state, null: false, default: 'active'
-      t.datetime :accepted_at
-      t.datetime :rejected_at
-      t.datetime :cancelled_at
-      t.numeric :amount, precision: 12, scale: 4, default: 0
-      t.string :currency, null: false, limit: 3
-      t.string :destination_country, null: false, limit: 3
-      t.numeric :shipping, precision: 12, scale: 4
-
-      t.timestamps
-    end
-
-    add_foreign_key :counter_offers, :listings
-    add_foreign_key :counter_offers, :accounts, column: :buyer_id
-    add_index :counter_offers, :listing_id
-    add_index :counter_offers, :buyer_id
-    add_index :counter_offers, :aasm_state
-
     add_column :listings, :accept_offers, :boolean, null: false, default: false
     add_column :listing_templates, :accept_offers, :boolean, null: false, default: false
   end

50726dfd6b56686508653c8f4d907af581832371
wivarn <wivarn@gmail.com>
2021-12-29 23:08:41 -0800

    change auto review schedule

--- app/jobs/order_job.rb
+++ app/jobs/order_job.rb
@@ -5,6 +5,7 @@ class OrderJob < ApplicationJob
   def mark_received
     Order.pending_shipment.or(Order.shipped).where('created_at <= ?', 30.days.ago).find_each do |order|
       order.receive!(order.buyer_id)
+      order.create_review(recommend: true, feedback: nil, reviewer: 'SYSTEM') unless order.review
       OrderMailer.received(order).deliver
     end
   end
@@ -16,16 +17,6 @@ class OrderJob < ApplicationJob
       14.days.ago, 15.days.ago
     ).find_each do |order|
       OrderMailer.review_reminder(order).deliver
-    end
-  end
-
-  rate '1 day'
-  def auto_review
-    Order.left_joins(:review).where(
-      'reviews.id IS NULL AND orders.received_at <= ? AND orders.received_at > ?',
-      14.days.ago, 15.days.ago
-    ).find_each do |order|
-      order.create_review(recommend: true, feedback: nil, reviewer: 'SYSTEM')
     end
   end
 end

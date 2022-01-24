8f1b7652fd21a6474a2463790d0fb37ef30746f3
wivarn <wivarn@gmail.com>
2022-01-24 15:22:18 -0800

    tweat prewarming

--- app/jobs/prewarm_job.rb
+++ app/jobs/prewarm_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PrewarmJob < ApplicationJob
-  rate '10 minutes'
+  rate '9 minutes'
   def full_request_warm
     puts URI.parse("https://api.#{ENV['DOMAIN']}/v0/listings/#{Listing.publically_viewable.first.id}").read
   end

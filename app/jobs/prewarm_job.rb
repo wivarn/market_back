899293d1cbfdc7f4dff7ee27395810a9ea0915da
wivarn <wivarn@gmail.com>
2022-01-24 14:31:30 -0800

    add missing file

---
+++ app/jobs/prewarm_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class PrewarmJob < ApplicationJob
+  rate '10 minutes'
+  def full_request_warm
+    puts URI.parse("https://api.#{ENV['DOMAIN']}/v0/listings/#{Listing.publically_viewable.first.id}").read
+  end
+end

b829f79738d24658d1d73b004d055a33025990eb
wivarn <wivarn@gmail.com>
2022-01-24 16:30:16 -0800

    5 min interval

--- app/jobs/prewarm_job.rb
+++ app/jobs/prewarm_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PrewarmJob < ApplicationJob
-  rate '9 minutes'
+  rate '5 minutes'
   def full_request_warm
     puts URI.parse("https://api.#{ENV['DOMAIN']}/v0/listings/#{Listing.publically_viewable.first.id}").read
   end

c422a9f0b75b39a75d460edb2f1d4aebb926e9d4
wivarn <wivarn@gmail.com>
2022-01-24 19:18:53 -0800

    hit frontend instead

--- app/jobs/prewarm_job.rb
+++ app/jobs/prewarm_job.rb
@@ -3,6 +3,6 @@
 class PrewarmJob < ApplicationJob
   rate '5 minutes'
   def full_request_warm
-    puts URI.parse("https://api.#{ENV['DOMAIN']}/v0/listings/#{Listing.publically_viewable.first.id}").read
+    puts URI.parse("#{ENV['FRONT_END_BASE_URL']}/listings/#{Listing.publically_viewable.last.id}").read
   end
 end

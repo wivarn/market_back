071715d49dfb86bb703afc1c876d32782cce133b
wivarn <wivarn@gmail.com>
2022-01-24 19:59:12 -0800

    return status code instead

--- app/jobs/prewarm_job.rb
+++ app/jobs/prewarm_job.rb
@@ -3,6 +3,8 @@
 class PrewarmJob < ApplicationJob
   rate '5 minutes'
   def full_request_warm
-    puts URI.parse("#{ENV['FRONT_END_BASE_URL']}/listings/#{Listing.publically_viewable.last.id}").read
+    uri = URI("#{ENV['FRONT_END_BASE_URL']}/listings/#{Listing.publically_viewable.last.id}")
+    res = Net::HTTP.get_response(uri)
+    puts res.code
   end
 end
